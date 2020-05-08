8de7025ab89f8a3f9645f324a15f20a68913edf0
Luis <xLuis1749@gmail.com>
2020-05-08 00:39:17 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+M.AutoInit()
+Vue.component('card',{
+    props: ['currencies'],
+    
+    template:`
+    <div>
+    <div v-for="currency in currencies.Data" class="card1" >
+            <h3>
+                <img :src="'https://cryptocompare.com'+currency.CoinInfo.ImageUrl" alt="">
+                {{currency.CoinInfo.FullName}} ({{currency.CoinInfo.Name}})
+            </h3>
+            <h6>Price: {{Math.round((currency.RAW.USD.PRICE*100)) /100}} USD 
+                <p style="color:red" v-if="currency.RAW.USD.CHANGEPCT24HOUR < 0"> {{Math.round((currency.RAW.USD.CHANGEPCT24HOUR *100) ) /100 }}% <i  class="fas fa-arrow-down" ></i></p>
+               <p style="color:green" v-else>{{Math.round((currency.RAW.USD.CHANGEPCT24HOUR *100) ) /100 }}% <i  class="fas fa-arrow-up" > </i> </p>
+             </h6>
+             <a  :href="'https://cryptocompare.com'+currency.CoinInfo.Url" class="btn blue" target='_blank' >Full Price change</a>
+        </div>
+        </div>
+    `
+})
+new Vue({
+    el: "#root",
+    data() {
+        return {
+            currencies: [],
+        }
+    },
+    mounted(){
+        this.getData()
+    },
+     methods: {
+        getData(){
+            axios.get('https://min-api.cryptocompare.com/data/top/totalvolfull?limit=52&tsym=USD').then(
+            response => {
+                
+                this.currencies = response.data
+                console.log(this.currencies)
+            })
+        },
+    },
+
+})
